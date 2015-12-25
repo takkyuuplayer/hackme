fb29f31f9fe93db8c2dccaa58458c763a1093cd3
takkyuuplayer <takkyuuplayer@gmail.com>
2015-12-25 13:38:51 +0000

    Add SQL injection

--- web/index.php
+++ web/index.php
@@ -4,6 +4,10 @@ require '../vendor/autoload.php';
 
 $markdown = "
 
+## SQL Injection
+
+* [Q. 01](sql-injection-01/index.php)
+
 ## Cross Site Request Forgery (CSRF)
 
 * [Q. 01](csrf-01/login.php)
