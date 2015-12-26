7a7e83cb893f57eb87c13016c2fc40c92bdbd765
takkyuuplayer <takkyuuplayer@gmail.com>
2015-12-26 15:21:41 +0000

    Fix typo

--- bin/mysql-setup.php
+++ bin/mysql-setup.php
@@ -3,5 +3,5 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 $pdo = \Hackme\Heroku\ClearDB::getConnection();
 
-$pdo->exec(file_get_contents(__DIR__ . '/../etc/schema/hackme.sql')) or die(print_r($db->errorInfo(), true));
-$pdo->exec(file_get_contents(__DIR__ . '/../etc/schema/dummy-data.sql')) or die(print_r($db->errorInfo(), true));
+$pdo->exec(file_get_contents(__DIR__ . '/../etc/schema/hackme.sql')) or die(print_r($pdo->errorInfo(), true));
+$pdo->exec(file_get_contents(__DIR__ . '/../etc/schema/dummy-data.sql')) or die(print_r($pdo->errorInfo(), true));
