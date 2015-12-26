7471f39799c442bc4cf1454ba86db6833cf3622e
takkyuuplayer <takkyuuplayer@gmail.com>
2015-12-26 14:32:41 +0000

    debug table names

--- bin/mysql-setup.php
+++ bin/mysql-setup.php
@@ -5,3 +5,7 @@ $pdo = \Hackme\Heroku\ClearDB::getConnection();
 
 $pdo->exec(file_get_contents(__DIR__ . '/../etc/schema/hackme.sql'));
 $pdo->exec(file_get_contents(__DIR__ . '/../etc/schema/dummy-data.sql'));
+
+foreach ($pdo->query('SHOW TABLES') as $row) {
+    print($row[0] . PHP_EOL);
+}
