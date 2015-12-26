5b620106e285b48095336ad1b66ae6ced37e7a79
takkyuuplayer <takkyuuplayer@gmail.com>
2015-12-26 04:21:17 +0000

    Add test for ClearDB

--- tests/01-unit/heroku-clear_dbTest.php
+++ tests/01-unit/heroku-clear_dbTest.php
@@ -1,7 +1,22 @@
 <?php
 class HerokuClearDBTest extends PHPUnit_Framework_TestCase
 {
-    public function testGetConfig()
+    public function testGetConfigWithClearDB()
+    {
+        putenv('CLEARDB_DATABASE_URL=mysql://username:password@hostname/dbname?reconnect=true');
+
+        $this->assertEquals(
+            \Hackme\Heroku\ClearDB::getConfig(),
+            [
+            'dsn'      => 'mysql:dbname=dbname;host=hostname;port=3306',
+            'user'     => 'username',
+            'password' => 'password',
+            'db'       => 'dbname',
+            ],
+            'w/ clear db'
+        );
+    }
+    public function testGetConfigWithoutClearDB()
     {
         $this->assertEquals(
             \Hackme\Heroku\ClearDB::getConfig(),
@@ -12,5 +27,10 @@ class HerokuClearDBTest extends PHPUnit_Framework_TestCase
             'db'       => 'hackme',
             ]
         );
+    }
+
+    protected function tearDown()
+    {
+        putenv('CLEARDB_DATABASE_URL');
     }
 }
